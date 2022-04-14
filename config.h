22b2f59ab7d1561e80c0c3f9664b72e0417eb90e
clawside <enyerplayus@gmail.com>
2022-04-14 19:44:10 -0400

    st: config.h: New fonts
    Signed-off-by: clawside <enyerplayus@gmail.com>

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Jetbrains Mono NF:pixelsize=12.5:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
